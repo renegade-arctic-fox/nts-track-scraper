0c1d0ccc7159a72b66281a8c8881830ea9200941
artemis <user@users-MacBook-Pro.local>
2024-02-07 13:06:23 +0000

    regex for show episodes url filter

--- nts-track-scraper.go
+++ nts-track-scraper.go
@@ -10,7 +10,7 @@ func main() {
 	c := colly.NewCollector(
 		colly.URLFilters(
 			regexp.MustCompile(
-				
+				`https://www.nts.live/shows/\S+/episodes/\S+-\d{1,2}\w{1,2}-\w+-\d{4}`,
 			),
 		),
 	)
