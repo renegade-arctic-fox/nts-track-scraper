758319082e4b0d662073494761dfbc66963ce3e6
Artemis SH <willowartemissh@gmail.com>
2024-02-09 00:11:10 +0000

    setup to visiting all links in specific genre set

--- nts-track-scraper.go
+++ nts-track-scraper.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"regexp"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
+	fmt.Println("Running...")
+
 	c := colly.NewCollector(
 		colly.URLFilters(
 			regexp.MustCompile(
-				`https://www.nts.live/shows/\S+/episodes/\S+-\d{1,2}\w{1,2}-\w+-\d{4}`,
+				`https:\/\/www\.nts\.live\/shows\/\S+\/episodes\/\S+-\d{1,2}\w{1,2}-\w+-\d{4}`,
+			),
+			regexp.MustCompile(
+				`https:\/\/www\.nts\.live\/explore\?genres\[\]=housetechno-techno&genres\[\]=housetechno-acid`,
 			),
 		),
 	)
 
-	c.
+	c.OnHTML("a[href]", func(h *colly.HTMLElement) {
+		link := h.Attr("href")
+
+		fmt.Printf("Link found: %q -> %s\n", h.Text, link)
+
+		c.Visit(h.Request.AbsoluteURL(link))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting...", r.URL.String())
+	})
+
+	c.Visit("https://www.nts.live/explore?genres[]=housetechno-techno&genres[]=housetechno-acid")
+
+	fmt.Println("Complete.")
 }
